d1da4364de68d763601940694845fdc4346a01b3
umarfarooq1147 <umarf@opiniion.com>
2023-03-23 21:53:19 +0500

    Code fix and some conditions added

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('buy/{cookies}', function ($cookies) {
+    $wallet = Auth::user()->wallet;
+    if ($cookies > $wallet) {
+        return "We donot have enough cookies to sell";
+    }
+    if ($wallet > 0) {
+        Auth::user()->update(['wallet' => $wallet - $cookies * 1]);
+        Log:info('User ' . Auth::user()->email . ' have bought ' . $cookies . ' cookies'); // we need to log who ordered and how much
+        return 'Success, you have bought ' . $cookies . ' cookies!';
+    }
+        return 'No cookie left';
+});
